8d28de1a0a7c3c2a0b03ab9127bc14c4192f2f20
Kuzniatsou Yauheni <yauheni_kuzniatsou@epam.com>
2024-11-13 17:26:14 +0300

    Add existing project files to Git

---
+++ BT_Off.bat
@@ -0,0 +1,16 @@
+@Echo Off
+set delay=5
+
+
+::wmic process where "commandline like '%script.bat%'" get name
+wmic process where "commandline like '%%Never_UPDATE.bat%%'" call terminate
+
+cls
+
+:start
+cls
+
+REM c:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe c:\1\MyScripts\ToggleDCupdate\QXAUpdateSwitcher\Always-Update-Dynamic-Conf-Update.ps1
+REM cd /d %~dp0
+c:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe %~dp0\switch-bt-Off.ps1
+
